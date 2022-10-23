6d78e6ddf95870eda184c28efe100a5021ce122f
APrakash30 <aprakash3004.jsr@gmail.com>
2022-10-23 23:32:28 +0530

    Updates

---
+++ precache-manifest.39b81af2b68cc06239a8489213572eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606fea9191ccce6eeae3d7fcad60f538",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "27d132f91b18b2500b2d",
+    "url": "/SnapShot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "dbe0e5c104a369e331c1",
+    "url": "/SnapShot/static/js/2.00509c9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnapShot/static/js/2.00509c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d132f91b18b2500b2d",
+    "url": "/SnapShot/static/js/main.a4e14733.chunk.js"
+  },
+  {
+    "revision": "17f44848bce1e5215d0e",
+    "url": "/SnapShot/static/js/runtime-main.136163d9.js"
+  }
+]);
